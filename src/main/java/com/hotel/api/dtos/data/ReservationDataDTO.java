dc5b18ebfaf50b5a190d888c56d151078c04c992
weesleycabral <weesleylima@hotmail.com>
2024-08-28 15:15:45 -0300

    chore: Update ReservationDataDTO to include room information in the record constructor

--- src/main/java/com/hotel/api/dtos/data/ReservationDataDTO.java
+++ src/main/java/com/hotel/api/dtos/data/ReservationDataDTO.java
@@ -2,12 +2,18 @@ package com.hotel.api.dtos.data;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.hotel.api.entities.Reservation;
+import com.hotel.api.entities.Room;
 
-public record ReservationDataDTO(LocalDate checkIn, LocalDate checkOut) {
+public record ReservationDataDTO(Room room,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy") LocalDate checkIn,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy") LocalDate checkOut) {
 
   public Reservation toEntity() {
-    return new Reservation(checkIn, checkOut);
+    Reservation reservation = new Reservation(room, checkIn, checkOut);
+    reservation.setRoom(this.room);
+    return reservation;
   }
 
 }
