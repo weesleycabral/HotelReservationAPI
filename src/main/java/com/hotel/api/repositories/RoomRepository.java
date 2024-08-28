6f5a8df02e0c527b074f114a0b929d1ef933053f
weesleycabral <weesleylima@hotmail.com>
2024-08-28 15:14:44 -0300

    feat: Update RoomRepository to use RoomType enum for findByRoomType method

--- src/main/java/com/hotel/api/repositories/RoomRepository.java
+++ src/main/java/com/hotel/api/repositories/RoomRepository.java
@@ -3,10 +3,11 @@ package com.hotel.api.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hotel.api.entities.Room;
+import com.hotel.api.enums.RoomType;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
   Room findByRoomNumber(String roomNumber);
 
-  Room findByRoomType(String roomType);
+  Room findByRoomType(RoomType roomType);
 
 }
