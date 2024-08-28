7cf7d781b077789db328fa2bc609243612f605e9
weesleycabral <weesleylima@hotmail.com>
2024-08-28 15:14:55 -0300

    feat: Update ReservationRepository to return a list of reservations by room ID

--- src/main/java/com/hotel/api/repositories/ReservationRepository.java
+++ src/main/java/com/hotel/api/repositories/ReservationRepository.java
@@ -1,11 +1,13 @@
 package com.hotel.api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hotel.api.entities.Reservation;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-  Reservation findByRoomId(Long roomId);
+  List<Reservation> findByRoomId(Long roomId);
 
   Reservation findByClientId(Long clientId);
 
