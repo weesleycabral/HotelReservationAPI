5a6b6ebd24f9d91a4f5434bd86f1b9f2940a5c97
weesleycabral <weesleylima@hotmail.com>
2024-08-28 15:12:35 -0300

    chore: Update pom.xml and application configuration for dotenv files

--- src/main/java/com/hotel/api/HotelReservationApplication.java
+++ src/main/java/com/hotel/api/HotelReservationApplication.java
@@ -3,10 +3,14 @@ package com.hotel.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class HotelReservationApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(HotelReservationApplication.class, args);
 	}
 
